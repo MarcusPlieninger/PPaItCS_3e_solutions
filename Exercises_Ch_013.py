ebd2a954e4cda51d94f91d7047665d7ad91abb82
Marcus Plieninger <mwp2101@columbia.edu>
2018-01-20 18:04:49 -0500

    Working on True/Flase

--- Exercises_Ch_013.py
+++ Exercises_Ch_013.py
@@ -8,11 +8,15 @@
 
 ## True/False
 
-#  1. Linear search requires a number of steps proportional to the size the list being searched. ?
+#  1. Linear search requires a number of steps proportional to the size of the list being searched. TRUE
+#     [Explanation: p. 464 "Let's consider linear search first. If we have a list of ten items, the most
+#     work our algorithm might have to do is to look at each item in turn. The loop will iterate at most
+#     ten times. Suppose the list is twice as big. Then it might have to look at twice as many items."]
 
 #  2. The python operator in performs a binary search. ?
 
-#  3. Binary search is an n log n algorithm. ?
+#  3. Binary search is an n log n algorithm. FALSE
+#     [Note: cf. 9.]
 
 #  4. The number of times n can be divided by 2 is exp(n). ?
 
@@ -22,7 +26,8 @@
 
 #  7. A word of length n has n! anagrams. TRUE
 
-#  8. Loops are more general than recursion. ?
+#  8. Loops are more general than recursion. FALSE
+#     [Explanation: p.474 "...recursive functions are a generalization of loops." 
 
 #  9. Merge sort is an example of n log n algorithm.
 
