c99df390af660945c9719184acd5ce0643478b0b
Marcus Plieninger <mwp2101@columbia.edu>
2018-01-20 13:48:02 -0500

    in progress

---
+++ Ch_1_exercises.py
@@ -0,0 +1,94 @@
+# Chapter 1 Exercises
+# "Python Programming: An Introduction to Computer Science, Third Edition" by John Zelle
+
+# Review Questions, pp. 21-2
+
+#  1. Computer science is the study of computers. FALSE
+#  2. The CPU is the "brain" of the computer. TRUE
+#  3. Secondary memory is also called RAM. FALSE
+#  4. All information that a computer is currently working on is stored in the main memory. TRUE
+#  5. The syntax of a language is its meaning, the semantics is its form. FALSE
+#  6. A function definition is a sequence of statements that defines a new command. TRUE
+#  7. A programming environment refers to a place where programmers work. FALSE
+#  8. A variable is used to give a name to a value so it can be referred to in other places. TRUE
+#  9. A loop is used to skip over a section of a program. FALSE
+# 10. A chaotic function can't be computed by a computer. FALSE
+#
+# Multiple Choice, pp. 22-3
+
+#  1. What is the fundamental question of computer science? b) What can be computed?
+#  2. An algorithm is like a d) recipe
+#  3. A problem is intractable when d) it is not practical to solve
+#  4. Which of the following is not an example of secondary memory a) RAM
+#  5. Computer languages designed to be used and understood by humans are b) high-level computer languages
+#  6. A statement is b) a complete computer command
+#  7. One difference between a compiler and an interpreter is c) a compiler is no longer needed after a program is translated
+#  8. By convention, the statements of a program are often placed in a function called b) main
+#  9. Which of the following is not true of comments? a) They make a program more efficient
+# 10. The items listed in the parentheses of a function are called d) both b) parameters and c) arguments are correct*
+#     *Note: I have a feeling that parameter refers more accurately to a *function definition* where as *argument* is used
+#     to refer to the value that is passed to the function when it is called, so that the argument replaces all instances
+#     of the parameter in the definition.
+
+# Discussion, pp. 23-4
+
+
+# Programming Exercises, pp. 24-5
+
+# 1. Start up an interactive Python session and try typing in each of the following commands. Write down the results you see there.
+#    a) print("Hello, world!")   | Expected result: 'Hello, world!' | Actual result:
+#    b) print("Hello", "world!") | Expected result: 'Hello' 'world!'| Actual result:
+#    c) print(3)                 | Expected result: 3               | Actual result:
+#    d) print(3,0)               | Expected result: '3,0'           | Actual result:
+#    e) print(2 + 3)             | Expected result: 5               | Actual result:
+#    f) print(2.0 + 3.0)         | Expected result: 5.0             | Actual result:
+#    g) print("2" + "3")         | Expected result: 23              | Actual result:
+#    h) print("2 + 3 =", 2 + 3)  | Expected result: 2 + 3 = 5       | Actual result:
+#    i) print(2 * 3)             | Expected result: 6               | Actual result:
+#    j) print(2 ** 3)            | Expected result: 8               | Actual result:
+#    k) print(7 / 3)             | Expected result: 2.3333333333335 | Actual result:
+#    l) print(7 // 3)            | Expected result: 2               | Actual result:
+
+# 2. Enter and run the chaos program from Section 1.6. Try it out with various values of input to see that it functions as described
+#    in the chapter.
+
+# 3. Modify the chaos program using 2.0 in place of 3.9 as the multiplier in the logistic function. Your modified line of code should
+#    look like this:
+#    x = 2.0 * x * (1 - x)
+#    Run the program for various input values and compare the results to those obtained from the original program. Write a short paragraph
+#    describing any differences that you notices in the behavior of the two versions.
+
+# 4. Modify the chaos program so that it prints out 20 values instead of 10.
+#    Solution:
+     # File: chaos.py
+     #A simple program illustrating chaotic behavior.
+     def main():
+     print("Ths program illustrates a chaotic function")
+     x = eval(input("Enter a number between 0 and 1: "))
+     for i in range (20):
+         x = 3.9 * x * (1 - x)
+         print(x)
+        
+# 5. Modfiy the chaos program so taht the number of values to print is determined by the user. You wil have to add a line near the top of the
+#    program to get another value from the user:
+#    n = eval(input("How many numbers should I print? "))
+#    Then you will need to change the loop to use n instead of a specific number.
+
+# 6. The calculation performed in the chaos program can be written in a number of ways that are algebraically equivalent. Write a version of
+#    the program for each of the following ways of doing the computation. Have your modified programs print out 100 iterations of the calculation
+#    and compare the results when run on the same input.
+#    a) 3.9 * x * (1 - x)
+#    b) 3.9 * x (x - x * x)
+#    c) 3.9 * x - 3.9 * x * x
+#    Explain the results of this experiment. Hint: See discussion question number 4, above.
+
+# 7. (Advanced) Modify the chaos program so that it accepts two inputs and then prints a table with two columns similar to the one shown in Section
+#    1.8. (Note: You will probably not be able to get the columns to line up as nicely as those in the example. Chapter 5 discusses how to print
+#    numbers with a fixed number of decimal places).
+
+
+
+
+
+
+
