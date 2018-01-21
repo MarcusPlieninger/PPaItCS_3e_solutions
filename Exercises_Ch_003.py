ca94fc2929b6d3ba5478c64bd1618348580cfd01
Marcus Plieninger <mwp2101@columbia.edu>
2018-01-21 15:00:10 -0500

    Working on T/F, 2 quesitons left

--- Exercises_Ch_003.py
+++ Exercises_Ch_003.py
@@ -9,23 +9,42 @@
 ## True/False
 
 #  1. Information that is stored and manipulated by computers is called data. TRUE
+#     [Explanation: p.57 "The information that is stored an amnipulated by computer program is
+#     gneerically rferred to as data. Different kinds of data will be stored and manipulated in
+#     different ways."
 
-#  2. Since floating-point numbers are extremely accurate, they generally be used instead of ints. ?
+#  2. Since floating-point numbers are extremely accurate, they generally be used instead of ints. FALSE
+#     [Explanation: p.59 "Another difference between ints and floats is hat the float type can only
+#     represent approximations of real numbers. As we will see, there is a limit to the precision, or
+#     accuracy of the stored values. Since float values are not exact, while ints always are, your general
+#     rule of thumb should be: If you don't need fractional values, use an int.]
 
 #  3. Operations like addition and subtraction are defined in the math library. FALSE
+#     [Explanaiton: p.60, see table of built-in numeric operations in Python. These include addition,
+#     subtraction, multiplication, float division, exponentiation, absolute value, integer division,
+#     and remainder.]
 
 #  4. The number of possible arrangements of n items is equalt to n!. TRUE
+#     [Explanation: p.68 With 6 items, there are 720 different ways of ordering them, i.e. 6!.
 
 #  5. The sqrt function computes the squirt of a number. FALSE
+#     [Explanation: The sqrt function computes the square root of a number.]
 
 #  6. The float data type is identical to the mathematical concept of a real number. ?
 
 #  7. Computers represent numbers using base-2 (binary) representations. TRUE
+#     [Explanation: p.72-3 "Computer memory is composed of electrical 'switches,' each of which can be
+#     in one of two possible states, basically on or off. Each switch represents a binary digit or bit
+#     of information. One bit can encode two possibilities, usually represented with the numberals 0 (for off)
+#     and 1 (for on)."
 
 #  8. A hardware float can represent a larger range of values than a hardware int. ?
 
 #  9. Type conversion functions such as float are a safe alternative to eval for getting a number
 #     as user input. TRUE
+#     [Explanation: p. 65 "Using int instead of eval in the input statements ensures that a user may
+#     only enter valid whole numbers. Any illegal (non-int) inputs will cause the program to crash with
+#     an erro message, thus avoiding the risk of a code injection attack...."]
 
 # 10. In Python, 4+5 produces the same result type as 4.0+5.0. FALSE
 
