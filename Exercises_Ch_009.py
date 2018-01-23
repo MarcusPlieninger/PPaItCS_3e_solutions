26a9b1f34006fd446016e862d8862108c739c724
Marcus Plieninger <mwp2101@columbia.edu>
2018-01-23 17:48:35 -0500

    T/F completed

--- Exercises_Ch_009.py
+++ Exercises_Ch_009.py
@@ -8,24 +8,47 @@
 ## True/False
 
 #  1. Computers can generate truly random numbers. FALSE
+#  [Explanation: Computers cannot generate randomnness, though they can simulate/approximate it.
+#  p.286: "Of course, you know that there is nothing random about computers; they are instruction-following
+#  machines."]
 
-#  2. The Python random function returns a pseudo-random int. ?
+#  2. The Python random function returns a pseudo-random int. FALSE
+#  [Explanation: p.287: "The random function can be used to generate pseudo-random floating-point values."]
 
-#  3. Top-down design is called stepwise refinement. ?
+#  3. Top-down design is called stepwise refinement. TRUE
+#  [Explanation: p.300: "You have just seen an example of top-down design in action. ... This approach is
+#  sometimes called stepwise refinement."]
 
 #  4. In top-down design, the main algorithm is written in terms of functions that don't yet exist. TRUE
+#  [Explanation: p.289: "Imagine that all the components you need to implement the algorithm have already been
+#  written for you. Your job is to finish this top-level algorithm using those components."
 
 #  5. The main function is at the top of a functional struture chart. TRUE
+#  [Explanation: p.292: See Figure 9.1 as an example.]
 
-#  6. A top-down design is best implemented from the top down. ?
+#  6. A top-down design is best implemented from the top down. FALSE
+#  [Explanation: p.301, see title for section 9.4, "Bottom-Up Implementation." "Just as designing a program
+#  one piece at a time is easier than trying to tackle the whole problem at once, implementation is best
+#  approached in small doses."]
 
-#  7. Unit-testing is the process of trying out a component of a larger program in isolation. TRUE
+#  7. Unit-testing is the process of trying out a component of a larger program in isolation. FALSE
+#  [Explanation: p.303: "We can continue this piecewise implementation, testing out each component as we add it
+#  into the code. Software engineers call this process unit testing. Testing each function independenly makes it
+#  easier to spot errors. By the time you get around to testing the entire program, chances are that
+#  everything will work smoothly."]
 
-#  8. A developer should use either top-down or spiral design, but not both. ?
+#  8. A developer should use either top-down or spiral design, but not both. FALSE
+#  [Explanation: p.306: "It is important to note that spiral development is not an alternative to top-down design.
+#  Rather, they are complementary approaches. When designing the prototype, you wil still use top-down techniques.
+#  In Chapter 12, you will see yet another approach called object-oriented design."]
 
 #  9. Reading design books alone will make you a great developer. FALSE
+#  [Explanation: p.306: "You can leaern about techniques by reading books like this one, but books can't teach when
+#  and how to apply them. That you have to learn for yourself through experience. In design, as in almost anything,
+#  the key to success is practice."
 
 # 10. A simplified version of a program is called a simulation. FALSE
+# [Explanation: p.304: "The initial stripped-down version is called a prototype."]
 
 
 ## Multiple Choice
