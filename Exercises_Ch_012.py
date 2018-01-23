8eb31fc555e1b4786baed1cf25296994922b7b86
Marcus Plieninger <mwp2101@columbia.edu>
2018-01-23 17:49:02 -0500

    T/F completed

--- Exercises_Ch_012.py
+++ Exercises_Ch_012.py
@@ -9,26 +9,42 @@
 ## True/False
 
 #  1. Object-oriented design is the process of finding and defining a useful set of functions for solving a problem. FALSE
-#     [Note: Sounds more like a definition related to functional programming.]
+#     [Note: Sounds more like a definition related to functional programming.
+#     Explanation: p.420: "Object-oriented design is the process of finding and defining a useful set of classes for
+#     a given problem."]
 
 #  2. Candidate objects can be found by looking at the verbs in a problem description. FALSE
-#     [Note: Going by intuition alone, objects should be based on nouns.]
+#     [Note: Going by intuition alone, objects should be based on nouns.
+#     Explanation: p.420: "Your goal is to define a set of objects that will be helpful in solving the problem. Start with a
+#     careful consideration of the problems statement. Objects are usually described as nouns."]
 
-#  3. Typically, the design process involves considerable trial and error. FALSE
-#     [I would think that it is implentation is the source of trial and error.]
+#  3. Typically, the design process involves considerable trial and error. TRUE
+#     [Explanation: p.421: "Don't be afraid to scrap an approach that doesn't seem to be working or to follow an idea and see
+#     where it leads. Good design involves a lot of trial and error."]
 
-#  4. GUIs are often built with a model-view architecture. ?
+#  4. GUIs are often built with a model-view architecture. TRUE
+#     [GUIs are concerned with the visible representation of a model, so yes.
+#     Explanation: p.519: "model-view architecture: Dividing up a GUI program by separating the problem (model) from the user
+#     interface (view)."]
 
 #  5. Hiding the details of an object in a class definition is called instantiation. FALSE
-#     [This is actually called encapsulation.]
+#     [This is actually called encapsulation.
+#     [Explanation: p.452: "The actual implementation of an object is independent of its use."
+#     p.516: "encapsulation: Hiding the details of something. Usually this is the term used to describe the distinction
+#     between the implementation and use of an object or function. Details are encapsulated in the definition."] 
 
-#  6. Polymorphism literally means "many changes." TRUE
+#  6. Polymorphism literally means "many changes." FALSE
+#     [Explanation: p.453: "Literally, the world polymorphism means 'many forms.'"]
 
 #  7. A superclass inherits behaviors from its subclasses. FALSE
-#     [Note: Again going by intuition here, inheritance works the other way.]
+#     [Note: Again going by intuition here, inheritance works the other way.
+#     Explanation: p.453: "The idea behind inheritance is that a new class can be defined to borrow behavior from another class.
+#     The new class (the one doing the borrowing) is called a subclass, and the existing class (the one being borrowed from) is
+#     its superclass."]
 
 #  8. GUIs are generally easier to write than text-based interfaces. FALSE
-#     [GUIs can be extremely complicated, hence my response.]
+#     [GUIs can be extremely complicated, hence my response.
+#     Explanation: p.440: "Usually, graphical interfaces are much more complicated to design and build than text-based ones."]
 
 
 ## Multiple Choice
