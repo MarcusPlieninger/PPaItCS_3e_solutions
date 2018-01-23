85e731416e8a19fbdb33327400b8d82a7a6a3713
Marcus Plieninger <mwp2101@columbia.edu>
2018-01-23 15:57:11 -0500

    T/F completed

--- Exercises_Ch_004.py
+++ Exercises_Ch_004.py
@@ -8,25 +8,48 @@
 
 ## True/False
 
-#  1. Using graphics.py allows graphics to be drawn in a Python shell window. TRUE
+#  1. Using graphics.py allows graphics to be drawn in a Python shell window. FALSE
+#     [Explanation: p.86: "The GraphWin() function creates a new window on the screen....The GraphWin
+#     may overlap your Python shell window, so you might have to resize or move the shell to make
+#     both windows fully visible."]
 
 #  2. Traditionally, the upper-left corner of a graphics window has coordinates (0,0). TRUE
+#     [Explanation: p.88: "Traditionally, graphics programmers locate the point (0,0) in the upper-left
+#     corner of the window,"]
 
 #  3. A single point on a graphics screen is called a pixel. TRUE
+#     [Explanation: p.88: "A graphics winow is actually a collection of tiny points called pixels
+#     (short for 'picture elements')."]
 
 #  4. A function that creates a new instance of a class is called an accessor. FALSE
+#     [Explanation: p.91: "To create a new instance of a class, we use a special operation called a
+#     constructor. A call to a constructor is an expression that creates a brand new object."
+#     p.92: Methods that "allows us to access information from the instance variables of the object"
+#     are called "accessors."]
 
-#  5. Instance variables are used to store data inside an object. ?
+#  5. Instance variables are used to store data inside an object. TRUE
+#     [Explanation: p.92: Values are stored inside an object as instance variables.]
 
-#  6. The statement myShape.move(10,20) moves myShape to the point (10,20). TRUE
+#  6. The statement myShape.move(10,20) moves myShape to the point (10,20). FALSE
+#     [Explanation: p.93: Specification of move method that all graphical objects have:
+#     "move(dx, dy): Moves the object dx units in the x direction and dy units in the y direction."
+#     Thus, the coordinates indicate movement, not position.]
 
 #  7. Aliasing occurs when two variables refer to the same object. TRUE
+#     [Explanation: p.95: "This situation where two vairabiles refer to the same object is called
+#     aliasing."]
 
-#  8. The copy method is provided to make a copy of a graphics object. TRUE
+#  8. The copy method is provided to make a copy of a graphics object. FALSE
+#     [Explanation: p.95: "The graphics library provides a better solution; all graphical objects support
+#     a clone method that makes a copy of the object."]
 
 #  9. A graphics window always has the title "Graphics Window." FALSE
+#     [Explanation: p.98: "...the GraphWin constructor allows an optional parameter to specify the title
+#     of the window."]
 
-# 10. The method in the graphics library used o get a mouse click is readMouse. ?
+# 10. The method in the graphics library used to get a mouse click is readMouse. FALSE
+#     [Explanation: p.107: "When getMouse is invoked on a GraphWin, the program pauses and waits for the
+#     user to click the mouse somewhere in the graphics window."
 
 
 ## Multiple Choice
