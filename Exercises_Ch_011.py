e11d330f93b8360163a8143f6a3bddcbfbd5bfcf
Marcus Plieninger <mwp2101@columbia.edu>
2018-01-23 18:34:03 -0500

    T/F completed

--- Exercises_Ch_011.py
+++ Exercises_Ch_011.py
@@ -8,23 +8,42 @@
 
 ## True/False
 
-#  1. The median is the average of a set of data. ?
+#  1. The median is the average of a set of data. FALSE
+#     [Explanation: p.365: It is the "middle value" in a set of numbers listed in order.
 
-#  2. Standard deviation measures how spread out a data set is. ?
+#  2. Standard deviation measures how spread out a data set is. TRUE
+#     [This definition is mostly true, except that it omits mention of mean (average).
+#     Explanation: p.365: "The standard deviation is a measure of how spread out the data is relative
+#     to the ean. If the data is tightly clustered around the mean, then the standard deviation is small.
+#     When the data is more spread out, the stnadard deviation is larger. The standard deviation
+#     provides a yardstick for determing how excpetional a value is."]
 
-#  3. Arrays are usually heterogeneous, but lists are homogeneous. ?
+#  3. Arrays are usually heterogeneous, but lists are homogeneous. FALSE
+#     [Explanation: Other way around.
+#     p.367: "Arrays are also usually homogeneous. ... [Python lists] are "heterogeneous."]
 
 #  4. A Python list cannot grow and shrink in size. FALSE
+#     [Explanation: p.367: "In contrast, Python lists are dynamic. They can grow and shrink on demand."]
 
 #  5. Unlike strings, Python lists are not mutable. FALSE
+#     [Explanation: p.368: "Recall that one important difference between lists and strings is that lists are
+#     mustable."]
 
-#  6. A list must contain at least one item. TRUE
+#  6. A list must contain at least one item. FALSE
+#     [Explanation: A list can start out empty, e.g if it is being used as an accumulator. See p.369.]
 
-#  7. Items can be removed from a list with the del operator. ?
+#  7. Items can be removed from a list with the del operator. TRUE
+#     [Explanation: p.369: "Individual items or entire slices can be removed from a list using the del operator."]
 
-#  8. A tuple is similar to an immutable list. ?
+#  8. A tuple is similar to an immutable list. TRUE
+#     [Explanation: p.387: "A tuple is just another kind of sequence in Python. Tuples are like lists except that
+#     tuples are immutable..."]
 
-#  9. A Python dictionary is a kind of sequence. ?
+#  9. A Python dictionary is a kind of sequence. FALSE
+#     [Explanation: p.401: "Python dictionaries are mappings [of key-value pairs]. Some other programming
+#     langauges provide similar structures called hashes or associative arrrays."]
+#     p.402: "When a dictionary is printed out, the order of keys will look essentially random. If you want
+#     to keep a collection of items in a certain order, you need a sequence, not a mapping."]
 
 
 ## Multiple Choice
