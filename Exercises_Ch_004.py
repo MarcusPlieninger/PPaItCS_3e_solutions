c036263103ee7f8e543bec0bc326311cba224758
Marcus Plieninger <mwp2101@columbia.edu>
2018-01-25 17:11:44 -0500

    in progress

--- Exercises_Ch_004.py
+++ Exercises_Ch_004.py
@@ -106,46 +106,76 @@
 
 #  3.  Describe what happens when the following interactive graphics program runs:
 #
-#      from graphics import *
+       from graphics import *
 #
-#      def main():                          # Defines the function named main() (i.e., the program)
-#          win = GraphWin()                 # Openstucts GraphWin object with "Graphics Window" default title and default size 200 x 200 pixels
-#          shape = Circle(Point(50,50), 20) # Constructs a circle object with center Point object at 50,50 and radius of 20 pixels
-#          shape.setOutline("red")          # Calls setOutline method of circle object named shape, seting its outline to the color red
-#          shape.setFill("red")             # Calls setFill method of circle object shape, it fills itself with the color red
-#          shape.draw(win)                  # Calls draw method of circle object named shape, it draws itself in graphics window object named win
-#          for i in range(10):              # Creates a counted loop that will iterate 10 times
-#              p = win.getMouse()           # Calls getMouse() method of graphics window object win, pauses program for user to click in win, returns mouse click as Point object,
-#                                           # which is assigned to the variable p (an event object)
-#              c = shape.getCenter()        # Calls getCenter() method of circle object shape, returns a clone of the center point of shape
-#              dx = p.getX() - c.getX()     # Assignment statement whereby variable dx is assigned the expression in the getX() methods of p and c are used to calculate the
+       def main():                          # Defines the function named main() (i.e., the program)
+           win = GraphWin()                 # Openstucts GraphWin object with "Graphics Window" default title and default size 200 x 200 pixels
+           shape = Circle(Point(50,50), 20) # Constructs a circle object with center Point object at 50,50 and radius of 20 pixels
+           shape.setOutline("red")          # Calls setOutline method of circle object named shape, seting its outline to the color red
+           shape.setFill("red")             # Calls setFill method of circle object shape, it fills itself with the color red
+           shape.draw(win)                  # Calls draw method of circle object named shape, it draws itself in graphics window object named win
+           for i in range(10):              # Creates a counted loop that will iterate 10 times
+               p = win.getMouse()           # Calls getMouse() method of graphics window object win, pauses program for user to click in win, returns mouse click as Point object,
+                                            # which is assigned to the variable p (an event object)
+               c = shape.getCenter()        # Calls getCenter() method of circle object shape, returns a clone of the center point of shape
+               dx = p.getX() - c.getX()     # Assignment statement whereby variable dx is assigned the expression in the getX() methods of p and c are used to calculate the
                                             # difference along the x axis of the user's mouse click with respect to the center point of shape
-#              dy = p.getY() - c.get&()     # Assignment statement whereby variable dy is assigned a similar expression as dx, except this time it is for the Y axis
-#              shape.move(dx, dy)           # Calls the move() method of shape to move shape by the x distance referred to by dx and the y distance referred to by dy, thus
-#                                           # changing the state of shape by moving its center point to the new coordinates encapsulated in the event object p
-#         win.close()                       # Calls close() method of graphics window object named, closing the graphics window after the loop is finished.
-#                                           # Note that it will not be possible to see the final location of the circle. To do so, comment out win.close() or call on win's
-#                                           # getMouse() method to make win close after a mouse click.
-#      main()                               # Calls the function named main() (i.e., the program).
+               dy = p.getY() - c.get&()     # Assignment statement whereby variable dy is assigned a similar expression as dx, except this time it is for the Y axis
+               shape.move(dx, dy)           # Calls the move() method of shape to move shape by the x distance referred to by dx and the y distance referred to by dy, thus
+                                            # changing the state of shape by moving its center point to the new coordinates encapsulated in the event object p
+          win.close()                       # Calls close() method of graphics window object named, closing the graphics window after the loop is finished.
+                                            # Note that it will not be possible to see the final location of the circle. To do so, comment out win.close() or call on win's
+                                            # getMouse() method to make win close after a mouse click.
+       main()                               # Calls the function named main() (i.e., the program).
 
 
 ### PROGRAMMING EXERCISES
 
 #  1. Alter the program from the last discussion question in the following ways:
 #     a) Make it draw squares instead of circles.
-#        Delete: shape = Circle(Point(50,50), 20)
-#        Replace: shape = Rectangle(Point(30,70), Point(70,30))
+#        Delete:
+         shape = Circle(Point(50,50), 20)
+#        Replace:
+         shape = Rectangle(Point(30,70), Point(70,30))
 #     b) Have each sucessive click draw an additional square on the screen (rather than moving the existing one).
-#        Delete:  shape.move(dx, dy)
-#        Replace: newshape = shape.clone()
-#                 newshape.move(dx,dy)
-#                 newshape.draw()
-#     c) Print a message on the window "Click again to quit" after the loop, and wait for a final clikc before closing the window.
-
+#        Delete:
+         shape.move(dx, dy)
+#        Replace:
+         newshape = shape.clone()
+         newshape.move(dx,dy)
+         newshape.draw()
+#     c) Print a message on the window "Click again to quit" after the loop, and wait for a final click before closing the window.
+#        Insert immediately before win.close():
+         quitmessage = Text(Point(100,100), "Click again to quit")
+         quitmessage.setSize(18)
+         quitmessage.setStyle("bold")
+         quitmessage.setTextColor("blue")
+         quitmessage.draw(win)
+         win.getMouse()
 
 #  2. An archery target consists of a central circle of yellow surrounded by concentric rings of red, bue, black and white. Each
 #     ring has the same width, which is the same as the radius of the yellow circle. Write a program that draws such a target.
 #     Hint: Objects drawn later will appear on top of objects drawn earlier.
+
+      from graphics import *
+
+      def main():
+          winwidth = 200
+          winlength = winwidth
+          r = (winwidth / 2)
+          center = Point((winwidth / 2),(winlength / 2))
+          color = ["white", "black", "blue", "red"] # a little advanced for this chapter, but it makes sense
+
+          win = GraphWin("Ready, Aim, Fire!", winwidth, winlength)
+
+          for i in range(4):
+        
+              aim = Circle(center, r - ((r * i)/4))
+              aim.setFill(color[i])
+              aim.draw(win)
+
+      main()
+
 
 #  3. Write a program that draws some sort of face.
 
